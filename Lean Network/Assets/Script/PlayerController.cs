3bfa3595a7e0c84acf59b1da7dd0408363e2298a
SardineFish <Sardine@live.cn>
2017-12-04 14:34:10 +0800

    Implement movement & first build

---
+++ Lean Network/Assets/Script/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+    public float VerticalMovment = 1;
+    public float HorizontalMovment = 1;
+    public float Jump = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * HorizontalMovment;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * VerticalMovment;
+
+        transform.Translate(x, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * Jump, ForceMode.Impulse);
+        }
+    }
+}
